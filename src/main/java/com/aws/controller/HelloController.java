c21a0e8e7a2ab96a2cf63680adeebb23ddf954ee
takumiKataoka-iFIELD <kataoka@ifield21.com>
2020-04-13 13:13:31 +0900

    initial commit

---
+++ src/main/java/com/aws/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.aws.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    String index(Model model) {
+        model.addAttribute("message","Springの復習");
+        return "index";
+    }
+}
